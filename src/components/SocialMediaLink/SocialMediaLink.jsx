a5d502624fc94e4d03dfdb785ec3b699a71b96ef
edumoreira1506 <contato@eduardoem.com.br>
2019-10-16 11:21:42 -0300

    Changed style of link

--- src/components/SocialMediaLink/SocialMediaLink.jsx
+++ src/components/SocialMediaLink/SocialMediaLink.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import './SocialMediaLink.scss'
 
 export default function index(props) {
   return (
-    <div className="navbar__entry" target="_blank">
+    <div className="navbar__entry SocialMediaLink" target="_blank">
       {props.to && (
         <a href={props.to}>
           {props.title}
